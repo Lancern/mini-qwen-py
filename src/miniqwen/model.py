4f2cb57fd9869849a1cb5a9035e79b0a23515934
Sirui Mu <msrlancern@gmail.com>
2025-05-02 20:41:02 +0800

    feat: decoder layers and model architecture

--- src/miniqwen/model.py
+++ src/miniqwen/model.py
@@ -7,6 +7,8 @@ import torch.nn as nn
 from safetensors import safe_open
 from transformers import Qwen2Tokenizer
 
+from .transformer import DecoderLayer, LayerNorm
+
 if TYPE_CHECKING:
     from os import PathLike
 
@@ -37,7 +39,9 @@ class RoPE(nn.Module):
         # inv_freq[i] = theta ** (-2i / head_dim)
         self._inv_freq = 1.0 / (theta ** (torch.arange(0, head_dim, 2) / head_dim))
 
-    def forward(self, x, position_ids):
+    def forward(
+        self, x: torch.Tensor, position_ids: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         # x :: (batch_size, seq_len, head_dim)
         # position_ids :: (batch_size, num_pos)
         # ret :: tuple
@@ -54,8 +58,10 @@ class RoPE(nn.Module):
         position_ids_expanded = position_ids[:, None, :].float()
         # position_ids_expanded :: (batch_size, 1, num_pos)
 
-        with torch.autocast(enabled=False):
-            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+        with torch.autocast(device_type=x.device.type, enabled=False):
+            freqs = (
+                inv_freq_expanded.float() @ position_ids_expanded.float()
+            ).transpose(1, 2)
             # freqs :: (batch_size, num_pos, head_dim / 2)
 
             emb = torch.cat((freqs, freqs), dim=-1)
@@ -70,47 +76,97 @@ class RoPE(nn.Module):
 
 class Model(nn.Module):
     def __init__(self, model_dir: "PathLike"):
+        super().__init__()
+
         with open(os.path.join(model_dir, "config.json"), "r", encoding="utf-8") as f:
             self._config = json.load(f)
+
+        self._vocab_size: int = self._config["vocab_size"]
+        self._rope_theta: float = self._config["rope_theta"]
+        self._hidden_size: int = self._config["hidden_size"]
+        self._head_dim: int = self._config["head_dim"]
+        self._num_attention_heads: int = self._config["num_attention_heads"]
+        self._num_key_value_heads: int = self._config["num_key_value_heads"]
+        self._intermediate_size: int = self._config["intermediate_size"]
+        self._rms_norm_eps: float = self._config["rms_norm_eps"]
+        self._num_hidden_layers: int = self._config["num_hidden_layers"]
 
         self._tokenizer = Qwen2Tokenizer.from_pretrained(model_dir)
         self._model_tensors = safe_open(
             os.path.join(model_dir, "model.safetensors"), framework="pt"
         )
 
-        # Load embedding weights
         self._embedding = Embedding(
-            self._config["vocab_size"], self._config["hidden_size"], self._model_tensors
+            self._vocab_size, self._hidden_size, self._model_tensors
         )
-        self._rope = RoPE(self._config["rope_theta"], self._config["head_dim"])
+        self._rope = RoPE(self._rope_theta, self._head_dim)
+        self._decoder_layers = [
+            DecoderLayer(
+                idx,
+                self._hidden_size,
+                self._head_dim,
+                self._num_attention_heads,
+                self._num_key_value_heads,
+                self._intermediate_size,
+                self._rms_norm_eps,
+                self._model_tensors,
+            )
+            for idx in range(self._num_hidden_layers)
+        ]
+        self._norm = LayerNorm(
+            "model.norm.weight",
+            self._hidden_size,
+            self._rms_norm_eps,
+            self._model_tensors,
+        )
+
+        self._lm_head = nn.Linear(self._hidden_size, self._vocab_size, bias=False)
+        self._lm_head.load_state_dict(
+            {"weight": self._model_tensors.get_tensor("lm_head.weight")}
+        )
 
     def generate(self, prompt: str) -> str:
-        input_ids = self._tokenizer(self._apply_chat_template(prompt), return_tensors="pt")[
-            "input_ids"
-        ]
+        input_ids = self._tokenizer(
+            self._apply_chat_template(prompt), return_tensors="pt"
+        )["input_ids"]
         # input_ids :: (1, seq_len)
 
         logits = self(input_ids)
-        # logits :: (1, 1, vocab_size)
+        # logits :: (1, seq_len, vocab_size)
+        print("input_ids shape:", input_ids.shape)
+        print("logits shape:", logits.shape)
 
         pass
 
-    def forward(self, input_ids):
+    def forward(self, input_ids: torch.Tensor):
         # input_ids :: (batch_size, seq_len)
+        # ret :: (batch_size, seq_len, vocab_size)
 
         batch_size = input_ids.shape[0]
         seq_len = input_ids.shape[1]
 
-        input_embedded = self._embedding(input_ids)
+        hidden_state = self._embedding(input_ids)
         # input_embedded :: (batch_size, seq_len, hidden_size)
-        assert input_embedded.shape == (batch_size, seq_len, self._config["hidden_size"])
+        assert hidden_state.shape == (
+            batch_size,
+            seq_len,
+            self._config["hidden_size"],
+        )
 
-        position_ids = torch.arange(0, seq_len, device=input_embedded.device).unsqueeze(0)
+        position_ids = torch.arange(0, seq_len, device=hidden_state.device).unsqueeze(0)
         # position_ids :: (1, seq_len)
-        position_embed = self._rope(input_embedded, position_ids)
-        # position_embed :: (batch_size, seq_len, hidden_size)
+        position_embeddings = self._rope(hidden_state, position_ids)
+        # position_embeddings :: tuple
+        #   => position_embeddings[0] :: (batch_size, seq_len, hidden_size)
+        #   => position_embeddings[1] :: (batch_size, seq_len, hidden_size)
 
-        pass
+        for decoder_layer in self._decoder_layers:
+            hidden_state = decoder_layer(hidden_state, position_embeddings)
+
+        hidden_state = self._norm(hidden_state)
+        # hidden_state :: (batch_size, seq_len, hidden_size)
+
+        return self._lm_head(hidden_state)
 
     def _apply_chat_template(self, prompt: str) -> str:
         return f"<|im_start|>user\n{prompt}<|im_end|>\n<|im_start|>assistant\n<think>\n\n</think>\n\n"
---
+++ src/miniqwen/transformer.py
@@ -0,0 +1,290 @@
+from safetensors import safe_open
+import torch
+import torch.nn as nn
+
+
+class SelfAttention(nn.Module):
+    def __init__(
+        self,
+        layer_idx: int,
+        hidden_size: int,
+        head_dim: int,
+        num_attention_heads: int,
+        num_kv_heads: int,
+        layernorm_eps: float,
+        model_tensors: safe_open,
+    ):
+        super().__init__()
+
+        self._head_dim = head_dim
+
+        self._q_proj = nn.Linear(
+            hidden_size, num_attention_heads * head_dim, bias=False
+        )
+        self._k_proj = nn.Linear(hidden_size, num_kv_heads * head_dim, bias=False)
+        self._v_proj = nn.Linear(hidden_size, num_kv_heads * head_dim, bias=False)
+        self._o_proj = nn.Linear(
+            num_attention_heads * head_dim, hidden_size, bias=False
+        )
+        self._q_norm = LayerNorm(
+            f"model.layers.{layer_idx}.self_attn.q_norm.weight",
+            head_dim,
+            layernorm_eps,
+            model_tensors,
+        )
+        self._k_norm = LayerNorm(
+            f"model.layers.{layer_idx}.self_attn.k_norm.weight",
+            head_dim,
+            layernorm_eps,
+            model_tensors,
+        )
+
+        # Load linear weights from model_tensors
+        self._q_proj.load_state_dict(
+            {
+                "weight": model_tensors.get_tensor(
+                    f"model.layers.{layer_idx}.self_attn.q_proj.weight"
+                )
+            }
+        )
+        self._k_proj.load_state_dict(
+            {
+                "weight": model_tensors.get_tensor(
+                    f"model.layers.{layer_idx}.self_attn.k_proj.weight"
+                )
+            }
+        )
+        self._v_proj.load_state_dict(
+            {
+                "weight": model_tensors.get_tensor(
+                    f"model.layers.{layer_idx}.self_attn.v_proj.weight"
+                )
+            }
+        )
+        self._o_proj.load_state_dict(
+            {
+                "weight": model_tensors.get_tensor(
+                    f"model.layers.{layer_idx}.self_attn.o_proj.weight"
+                )
+            }
+        )
+
+    def forward(
+        self, x: torch.Tensor, position_embeddings: tuple[torch.Tensor, torch.Tensor]
+    ):
+        # x :: (batch_size, seq_len, hidden_size)
+        # position_embeddings[0] ::
+
+        input_shape = x.shape[:-1]
+        hidden_shape = (*input_shape, -1, self._head_dim)
+        # hidden_shape = (batch_size, seq_len, -1, self._head_dim)
+
+        q_states = self._q_norm(self._q_proj(x).view(hidden_shape)).transpose(1, 2)
+        # q_states :: (batch_size, num_attention_heads, seq_len, head_dim)
+        k_states = self._k_norm(self._k_proj(x).view(hidden_shape)).transpose(1, 2)
+        # k_states :: (batch_size, num_kv_heads, seq_len, head_dim)
+        v_states = self._v_proj(x).view(hidden_shape).transpose(1, 2)
+        # v_states :: (batch_size, num_kv_heads, seq_len, head_dim)
+
+        cos, sin = position_embeddings
+        q_states, k_states = self._rotary_pos_embed(q_states, k_states, cos, sin)
+
+        attn_output = self._attend(q_states, k_states, v_states)
+        # attn_output :: (batch_size, seq_len, num_attention_heads, head_dim)
+
+        attn_output = attn_output.reshape(*input_shape, -1).contiguous()
+        # attn_output :: (batch_size, seq_len, num_attention_heads * head_dim)
+        attn_output = self._o_proj(attn_output)
+        # attn_output :: (batch_size, seq_len, hidden_size)
+
+        return attn_output
+
+    def _rotary_pos_embed(
+        self, q: torch.Tensor, k: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        # q :: (batch_size, num_attention_heads, seq_len, head_dim)
+        # k :: (batch_size, num_kv_heads, seq_len, head_dim)
+
+        def rotate_half(x):
+            # x :: (..., d)
+            x1 = x[..., : x.shape[-1] // 2]
+            x2 = x[..., x.shape[-1] // 2 :]
+            # x1, x2 :: (..., d / 2)
+            return torch.cat((-x2, x1), dim=-1)
+
+        cos = cos.unsqueeze(1)
+        sin = sin.unsqueeze(1)
+        q_embed = (q * cos) + (rotate_half(q) * sin)
+        k_embed = (k * cos) + (rotate_half(k) * sin)
+        return q_embed, k_embed
+
+    def _attend(
+        self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor
+    ) -> torch.Tensor:
+        # q :: (batch_size, num_attention_heads, seq_len, head_dim)
+        # k :: (batch_size, num_kv_heads, seq_len, head_dim)
+        # v :: (batch_size, num_kv_heads, seq_len, head_dim)
+
+        # Make the shape of k and v the same as q.
+        batch_size, num_kv_heads, seq_len, head_dim = k.shape
+        num_attention_heads = q.shape[1]
+        num_kv_groups = num_attention_heads // num_kv_heads
+
+        def expand_kv(x: torch.Tensor) -> torch.Tensor:
+            if num_kv_groups <= 1:
+                return x
+            x = x[:, :, None, :, :].expand(
+                batch_size, num_kv_heads, num_kv_groups, seq_len, head_dim
+            )
+            return x.reshape(batch_size, num_attention_heads, seq_len, head_dim)
+
+        k = expand_kv(k)
+        v = expand_kv(v)
+        # k :: (batch_size, num_attention_heads, seq_len, head_dim)
+        # v :: (batch_size, num_attention_heads, seq_len, head_dim)
+
+        scaling = self._head_dim**-0.5
+        attn_weights = torch.matmul(q, k.transpose(2, 3)) * scaling
+        # attn_weights :: (batch_size, num_attention_heads, seq_len, seq_len)
+
+        attn_weights = nn.functional.softmax(
+            attn_weights, dim=-1, dtype=torch.float32
+        ).to(q.dtype)
+        # attn_weights :: (batch_size, num_attention_heads, seq_len, seq_len)
+
+        attn_output = torch.matmul(attn_weights, v)
+        # attn_output :: (batch_size, num_attention_heads, seq_len, head_dim)
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        # attn_output :: (batch_size, seq_len, num_attention_heads, head_dim)
+
+        return attn_output
+
+
+class MLP(nn.Module):
+    def __init__(
+        self,
+        layer_idx: int,
+        hidden_size: int,
+        intermediate_size: int,
+        model_tensors: safe_open,
+    ):
+        super().__init__()
+
+        self._gate_proj = nn.Linear(hidden_size, intermediate_size, bias=False)
+        self._up_proj = nn.Linear(hidden_size, intermediate_size, bias=False)
+        self._down_proj = nn.Linear(intermediate_size, hidden_size, bias=False)
+        self._act_fn = nn.SiLU()
+
+        # Load weights from model_tensors
+        self._gate_proj.load_state_dict(
+            {
+                "weight": model_tensors.get_tensor(
+                    f"model.layers.{layer_idx}.mlp.gate_proj.weight"
+                )
+            }
+        )
+        self._up_proj.load_state_dict(
+            {
+                "weight": model_tensors.get_tensor(
+                    f"model.layers.{layer_idx}.mlp.up_proj.weight"
+                )
+            }
+        )
+        self._down_proj.load_state_dict(
+            {
+                "weight": model_tensors.get_tensor(
+                    f"model.layers.{layer_idx}.mlp.down_proj.weight"
+                )
+            }
+        )
+
+    def forward(self, x: torch.Tensor):
+        # x :: (batch_size, seq_len, hidden_size)
+        # ret :: (batch_size, seq_len, hidden_size)
+        return self._down_proj(self._act_fn(self._gate_proj(x)) * self._up_proj(x))
+
+
+class LayerNorm(nn.Module):
+    def __init__(
+        self,
+        name: str,
+        hidden_size: int,
+        eps: float,
+        model_tensors: safe_open,
+    ):
+        super().__init__()
+
+        self._hidden_size = hidden_size
+        self._eps = eps
+
+        self._weight = model_tensors.get_tensor(name)
+
+    def forward(self, x: torch.Tensor):
+        # x :: (batch_size, seq_len, hidden_size)
+        # ret :: (batch_size, seq_len, hidden_size)
+        input_dtype = x.dtype
+        x = x.float()
+        var = x.pow(2).mean(-1, keepdim=True)
+        x = x * torch.rsqrt(var + self._eps)
+        return self._weight * x.to(input_dtype)
+
+
+class DecoderLayer(nn.Module):
+    def __init__(
+        self,
+        layer_idx: int,
+        hidden_size: int,
+        head_dim: int,
+        num_attention_heads: int,
+        num_kv_heads: int,
+        intermediate_size: int,
+        layernorm_eps: float,
+        model_tensors: safe_open,
+    ):
+        super().__init__()
+
+        self._hidden_size = hidden_size
+
+        self._self_attn = SelfAttention(
+            layer_idx,
+            hidden_size,
+            head_dim,
+            num_attention_heads,
+            num_kv_heads,
+            layernorm_eps,
+            model_tensors,
+        )
+        self._mlp = MLP(layer_idx, hidden_size, intermediate_size, model_tensors)
+        self._input_layernorm = LayerNorm(
+            f"model.layers.{layer_idx}.input_layernorm.weight",
+            hidden_size,
+            layernorm_eps,
+            model_tensors,
+        )
+        self._post_att_layernorm = LayerNorm(
+            f"model.layers.{layer_idx}.post_attention_layernorm.weight",
+            hidden_size,
+            layernorm_eps,
+            model_tensors,
+        )
+
+    def forward(
+        self, x: torch.Tensor, position_embeddings: tuple[torch.Tensor, torch.Tensor]
+    ):
+        # x :: (batch_size, seq_len, hidden_size)
+        # ret :: (batch_size, seq_len, hidden_size)
+        residual = x
+
+        x = self._input_layernorm(x)
+        # x :: (batch_size, seq_len, hidden_size)
+
+        x = self._self_attn(x, position_embeddings)
+        x = x + residual
+        # x :: (batch_size, seq_len, hidden_size)
+
+        residual = x
+        x = self._mlp(self._post_att_layernorm(x))
+        x = x + residual
+        # x :: (batch_size, seq_len, hidden_size)
+
+        return x
