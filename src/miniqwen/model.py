eb2a8fa83dab7142d784ddd58b0ab44dc3d9660a
Sirui Mu <msrlancern@gmail.com>
2025-05-02 13:12:32 +0800

    feat: rotary embedding

--- src/miniqwen/model.py
+++ src/miniqwen/model.py
@@ -37,10 +37,35 @@ class RoPE(nn.Module):
         # inv_freq[i] = theta ** (-2i / head_dim)
         self._inv_freq = 1.0 / (theta ** (torch.arange(0, head_dim, 2) / head_dim))
 
-    def forward(self, x):
+    def forward(self, x, position_ids):
         # x :: (batch_size, seq_len, head_dim)
-        # ret :: (batch_size, seq_len, head_dim)
-        pass
+        # position_ids :: (batch_size, num_pos)
+        # ret :: tuple
+        #   => ret[0] :: (batch_size, num_pos, head_dim)
+        #   => ret[1] :: (batch_size, num_pos, head_dim)
+
+        batch_size = x.shape[0]
+
+        inv_freq_expanded = (
+            self._inv_freq[None, :, None].float().expand(batch_size, -1, 1)
+        )
+        # inv_freq_expanded :: (batch_size, head_dim / 2, 1)
+
+        position_ids_expanded = position_ids[:, None, :].float()
+        # position_ids_expanded :: (batch_size, 1, num_pos)
+
+        with torch.autocast(enabled=False):
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+            # freqs :: (batch_size, num_pos, head_dim / 2)
+
+            emb = torch.cat((freqs, freqs), dim=-1)
+            # emb :: (batch_size, num_pos, head_dim)
+
+            cos = emb.cos()
+            sin = emb.sin()
+            # cos, sin :: (batch_size, num_pos, head_dim)
+
+        return cos.to(x.dtype), sin.to(x.dtype)
 
 
 class Model(nn.Module):
@@ -60,18 +85,31 @@ class Model(nn.Module):
         self._rope = RoPE(self._config["rope_theta"], self._config["head_dim"])
 
     def generate(self, prompt: str) -> str:
-        input = self._tokenizer(self._apply_chat_template(prompt), return_tensors="pt")[
+        input_ids = self._tokenizer(self._apply_chat_template(prompt), return_tensors="pt")[
             "input_ids"
         ]
-        seq_len = input.shape[1]
-        # input :: (1, seq_len)
+        # input_ids :: (1, seq_len)
 
-        input_embedded = self._embedding(input)
-        # input_embedded :: (1, seq_len, hidden_size)
-        assert input_embedded.shape == (1, seq_len, self._config["hidden_size"])
+        logits = self(input_ids)
+        # logits :: (1, 1, vocab_size)
 
-    def forward(self, input):
-        # input :: (batch_size, seq_len, hidden_size)
+        pass
+
+    def forward(self, input_ids):
+        # input_ids :: (batch_size, seq_len)
+
+        batch_size = input_ids.shape[0]
+        seq_len = input_ids.shape[1]
+
+        input_embedded = self._embedding(input_ids)
+        # input_embedded :: (batch_size, seq_len, hidden_size)
+        assert input_embedded.shape == (batch_size, seq_len, self._config["hidden_size"])
+
+        position_ids = torch.arange(0, seq_len, device=input_embedded.device).unsqueeze(0)
+        # position_ids :: (1, seq_len)
+        position_embed = self._rope(input_embedded, position_ids)
+        # position_embed :: (batch_size, seq_len, hidden_size)
+
         pass
 
     def _apply_chat_template(self, prompt: str) -> str:
